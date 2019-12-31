677aba26ebc4161d4d172a1019ddc5ca313f8770
Tim <tim@tim-ings.com>
2019-12-31 17:22:06 +0800

    Implements arg parsing and -C, -p, -f

--- src/main.c
+++ src/main.c
@@ -10,15 +10,60 @@
 #include "str.h"
 
 
-int main(int argc, char** argv) {
-    // parse the bakefile
-    BakeFile* bake = BakeFile_new(NULL);
-    // BakeFile_print(bake);
+struct CLARGS {
+    char* directory;
+    char* filename;
+    bool ignore_failed_actions;
+    bool dry_run;
+    bool print_bakefile;
+    bool silent;
+} typedef CLARGS;
 
-    // run the bakefile
+CLARGS parse_args(int argc, char** argv) {
+    CLARGS clargs;
+    // defaults
+    clargs.directory = NULL;
+    clargs.filename = NULL;
+    clargs.ignore_failed_actions = false;
+    clargs.dry_run = false;
+    clargs.print_bakefile = false;
+    clargs.silent = false;
+    // go over every arg 
+    for (int i = 0; i < argc; i++) {
+        char* v = argv[i];
+        if (strcmp(v, "-C") == 0) {
+            clargs.directory = argv[++i];
+        } else if (strcmp(v, "-f") == 0) {
+            clargs.filename = argv[++i];
+        } else if (strcmp(v, "-i") == 0) {
+            clargs.ignore_failed_actions = true;
+        } else if (strcmp(v, "-n") == 0) {
+            clargs.dry_run = true;
+        } else if (strcmp(v, "-p") == 0) {
+            clargs.print_bakefile = true;
+        } else if (strcmp(v, "-s") == 0) {
+            clargs.silent = true;
+        }
+    }
+    return clargs;
+}
+
+int main(int argc, char** argv) {
+    CLARGS clargs = parse_args(argc, argv);
+
+    if (clargs.directory != NULL) {
+        chdir(clargs.directory);
+    }
+
+    BakeFile* bake = BakeFile_new(NULL);
+
+    if (clargs.print_bakefile) {
+        BakeFile_print(bake);
+        exit(EXIT_SUCCESS);
+    }
+
     BakeFile_run(bake);
     
-    // free resources and exit
     BakeFile_free(bake);
     return EXIT_SUCCESS;
 }
